bde98b3dcc96e0f37cf4b697358341ca343a3a6e
kamitail <92752516+kamitail@users.noreply.github.com>
2024-01-26 22:38:15 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const PLAYLIST_API_ENDPOINT = 'https://playlists-api.vercel.app';
-export const PLAYLISTS_WEBSOCKET_ENDPOINT = 'ws://localhost:7071/ws';
+export const PLAYLISTS_WEBSOCKET_ENDPOINT = 'wss://playlists-socket.onrender.com';
 export const USERS_TAB_INDEX = 2;
